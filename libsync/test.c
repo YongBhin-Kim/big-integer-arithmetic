4cff05c7dcdcaba57d248915e812af0a532dad18
YongBhinKim <coji67@gmail.com>
2023-10-11 15:05:15 +0900

    test code

--- libsync/test.c
+++ libsync/test.c
@@ -1,5 +1,9 @@
 #include "bigint.h"
 
+// march oview open source
+
+// 1. rng drbg 추가 및 의존성 제거 (AES_CTR_DRBG)
+// 2. 
 
 int main() {
 
@@ -9,37 +13,37 @@ int main() {
 
     word *a = NULL;
 
-    // test : gen new array and show array (yb)
+    // test : gen new array and show array
     array_new_rand(&a, 5);
     printf("a = "); array_show_hex(a, 5);
-    printf("----------\n");
+    printf("---------------------------------------------\n");
 
-    // test : bigint <- array (yb)
+    // test : bigint <- array
     bi_set_by_array(&x, NEGATIVE, a, 5);
     printf("x = "); bi_show_hex(x);
-    printf("----------\n");
+    printf("---------------------------------------------\n");
 
-    // test : gen rand bigint (yb)
+    // test : gen rand bigint
     bi_gen_rand(&y, NEGATIVE, 10);
     printf("y = "); bi_show_hex(y);
-    printf("----------\n");
+    printf("---------------------------------------------\n");
 
-    // test : bigint <- bigint assign (yb)
+    // test : bigint <- bigint assign
     bi_assign(&z, y);
     printf("z = "); bi_show_hex(z);
-    printf("----------\n");
+    printf("---------------------------------------------\n");
 
-    // test : print binary (yb)
+    // test : print binary
     printf("z = "); bi_show_bin(z);
 
-    // test : isone, iszero (yb)
+    // test : isone, iszero
     bi_set_zero(&x);
     printf("x is zero? %d\n", (bi_is_zero(&x) == TRUE));
     bi_set_one(&x);
     printf("x is one? %d\n", (bi_is_one(&x) == TRUE));
-    printf("----------\n");
+    printf("---------------------------------------------\n");
 
-    // test : compare (yb)
+    // test : compare
     y->sign = NON_NEGATIVE;
     z->sign = NON_NEGATIVE;
     
@@ -48,9 +52,9 @@ int main() {
     printf("z = "); bi_show_hex(z);
     printf("x VS y = %d\n", bi_compare(&x, &y)); // x > y (1)
     printf("y VS z = %d\n", bi_compare(&y, &z)); // x = y (0)
-    printf("----------\n");
+    printf("---------------------------------------------\n");
 
-    // test : bi_shift (yb)
+    // test : bi_shift
     printf("z = "); bi_show_hex(z);
     bi_shift_right(&z, 64);
     printf(" >>= 64 = "); bi_show_hex(z);
@@ -73,15 +77,29 @@ int main() {
     bi_shift_left(&z, 200);
     printf(" <<= 200 = "); bi_show_hex(z);
 
-    printf("----------\n");
+    printf("---------------------------------------------\n");
 
-    // test : bi_reduction (yb)
+    // test : bi_reduction
     printf("z = "); bi_show_hex(z);
     if ( bi_reduction(&z, 400) == 1 ) {
         printf(" mod 2^400 = "); bi_show_hex(z);
     }
 
-    printf("----------\n");
+    printf("---------------------------------------------\n");
+
+    // test : bi_add
+    bi_set_zero(&x);
+    bi_gen_rand(&x, NON_NEGATIVE, 10);
+    bi_gen_rand(&y, NON_NEGATIVE, 10);
+    x->a[9]=0xffffffff;x->a[8]=0xffffffff;y->a[9]=0xffffffff;y->a[8]=0xffffffff;
+    printf("x = "); bi_show_hex(x);
+    printf("y = "); bi_show_hex(y);
+
+    bi_add(&z, x, y);
+    printf("z = x + y = "); bi_show_hex(z);
+    
+    // test : bi_sub
+
 
     bi_delete(&x);
     bi_delete(&y);
