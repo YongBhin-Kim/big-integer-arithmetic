d1520ca614fe414910ca02ea8b8ce1bfdeb01dc3
YongBhinKim <coji67@gmail.com>
2023-11-09 16:27:37 +0900

    big integer array headers

--- libsync/array.h
+++ libsync/array.h
@@ -9,8 +9,18 @@
 
 typedef unsigned int word;
 typedef unsigned char byte;
+#ifndef WORD_BITS
 #define WORD_BITS (sizeof(word)*8)
+#endif
+#ifndef WORD_BYTES
 #define WORD_BYTES (sizeof(word))
+#endif
+#ifndef MAX
+#define MAX(a,b) (((a) > (b)) ? (a) : (b))
+#endif
+#ifndef MIN
+#define MIN(a,b) (((a) < (b)) ? (a) : (b))
+#endif
 
 void array_init(word *a, const size_t wordlen);
 void array_copy(word *dst, const word *src, const size_t wordlen);
